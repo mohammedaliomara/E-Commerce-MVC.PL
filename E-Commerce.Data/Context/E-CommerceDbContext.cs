81b5ef6af9b429751177c3c5f64e3d261f189f50
MoHannad1223 <mohanedemam408@gmail.com>
2025-03-27 23:25:33 +0200

    mohammed ali

---
+++ E-Commerce.Data/Context/E-CommerceDbContext.cs
@@ -0,0 +1,28 @@
+﻿using E_Commerce.Data.DataOrEntity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Data.Context
+{
+    public class E_CommerceDbContext:DbContext
+    {
+        public E_CommerceDbContext(DbContextOptions<E_CommerceDbContext> options):base(options) 
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Product> products { get; set; }
+        public DbSet<ProductBrand> productBrands { get; set; }
+        public DbSet<ProductType> productTypes { get; set; }
+
+    }
+}
---
+++ E-Commerce.Data/DataOrEntity/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Data.DataOrEntity
+{
+   public class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+}
